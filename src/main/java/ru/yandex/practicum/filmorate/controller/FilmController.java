25545c70fb3ff5a2e08a5a2c38ee8ac86ce87fa3
Andrey <andrei4-09@mail.ru>
2023-04-05 08:37:20 +0300

    bugfix: Внесены правки согласно ревью

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 
 import org.springframework.web.bind.annotation.*;
 
