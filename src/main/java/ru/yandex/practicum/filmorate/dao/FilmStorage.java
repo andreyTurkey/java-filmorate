d1ad40e6ea5c9bbeb51a0aa43b87dbb1f712b631
Andrey <andrei4-09@mail.ru>
2023-05-05 16:34:58 +0300

    Удалены лишние импорты

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmStorage.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.dao;
-
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Rating;
+
 
 import java.util.List;
 
@@ -16,27 +14,11 @@ public interface FilmStorage {
 
     Film getFilmById(Integer id);
 
-    //Genre getGenreById(Integer id);
-
-    //List<Genre> getAllGenres();
-
-    //Rating getRatingById(Integer id);
-
-    //List<Rating> getAllRating();
-
     void filmExistsById(Integer filmId);
-
-    //void genreExistsById(Integer genreId);
-
-    //void ratingExistsById(Integer ratingId);
 
     Film addLike(Integer filmId, Integer userId);
 
     Film deleteLike(Integer filmId, Integer userId);
 
     List<Film> getFilmByRating(Integer count);
-
-    //void updateFilmGenre(Film film);
-
-    //List<Genre> getGenresByFilmId(Integer id);
 }
